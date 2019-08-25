08199d14be1a848dad647b48df05fe8e27210def
r0ck3r008 <naman.arora@tutanota.com>
2019-08-24 23:16:54 -0400

    globally defined the srv_sock and prototype for the signal callback function

--- src/dir_sv/udp_workings.h
+++ src/dir_sv/udp_workings.h
@@ -5,7 +5,10 @@
 #include<netinet/in.h>
 #include<arpa/inet.h>
 
+int srv_sock;
+
 int sock_create(char *);
+void sig_callback(int);
 void udp_workings(char *);
 char *rcv(int, struct sockaddr_in **);
 void snd(int, char *, struct sockaddr_in *);
