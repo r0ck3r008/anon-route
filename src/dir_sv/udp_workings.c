fc10db4ce3360e64baf0ae9197e67714c72876b2
r0ck3r008 <naman.arora@tutanota.com>
2019-08-24 22:10:06 -0400

    added struct sockaddr_in defining headers in udp_workings.h and removed from udp_workings.c

--- src/dir_sv/udp_workings.c
+++ src/dir_sv/udp_workings.c
@@ -1,9 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<arpa/inet.h>
 #include<unistd.h>
 #include<errno.h>
 
--- src/dir_sv/udp_workings.h
+++ src/dir_sv/udp_workings.h
@@ -1,6 +1,10 @@
 #ifndef UDP_WORKINGS_H
 #define UDP_WORKINGS_H
 
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+
 int sock_create(char *);
 void udp_workings(char *);
 char *rcv(int, struct sockaddr_in **);
