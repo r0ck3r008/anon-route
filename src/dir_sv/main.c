1a08d78d56971d88ce2c98c0daeb3be661225bbf
r0ck3r008 <naman.arora@tutanota.com>
2019-08-24 22:09:27 -0400

    main function to call initiate the direcotry service

--- src/dir_sv/main.c
+++ src/dir_sv/main.c
@@ -1,13 +1,8 @@
 #include<stdio.h>
 
-#include"h_map.h"
+#include"udp_workings.h"
 
 int main(int argc, char *argv[])
 {
-	struct h_map_t *h_map=h_map_alloc(10);
-
-	h_map_add(h_map, argv[1], 0);
-	h_map_add(h_map, argv[1], 23);
-
-	h_map_dealloc(h_map);
+	udp_workings(argv[1]);
 }
